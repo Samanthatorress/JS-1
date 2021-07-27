68a884d93567c440a9118a4319ec433b4f9048bc
SammyTorres <87827499+Samanthatorress@users.noreply.github.com>
2021-07-27 17:46:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+/*var numberOfApples = 3;
+var applesPicked = 1;
+var total = numberOfApples + applesPicked;
+console.log ("There are " + total + " apples in the basket.");*/
+
+/*var numberOfApples = prompt("How many apples are in the basket?");
+var applesPicked = prompt("How many apples were picked?");
+numberOfApples =parseInt(numberOfApples);
+applesPicked =parseInt(applesPicked);
+var total = numberOfApples + applesPicked;
+console.log("There are " + total + " apples in the basket.");*/
+
+/*var totalMiles = prompt("How many miles did you run?");
+var timePerMile = prompt("How fast did you run each mile in minutes?");
+totalMiles = parseFloat(totalMiles);
+timePerMile = parseFloat(timePerMile);
+var result = totalMiles * timePerMile;
+console.log ("You spent " + result + " minutes running.");*/
+
+/*main();
+
+function muffinCalculator() {
+  var chocolateMuffins = prompt("How many muffins do you have?");
+  var muffinsEaten = prompt("How many did you eat?");
+  chocolateMuffins = parseInt(chocolateMuffins);
+  muffinsEaten = parseInt(muffinsEaten);
+  var result = chocolateMuffins - muffinsEaten;
+  console.log ("You have " + result + " muffins left.");
+}
+
+function main() {
+  muffinCalculator();
+  var userAnswer = prompt("do you want to play again? Y/N");
+  if (userAnswer == "Y"){
+    main();
+  } else {
+    console.log("Thank you for using my calculator!");
+  }
+}*/
